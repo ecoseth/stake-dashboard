f0cd9216acd10d8263bbf82c7ff599dae2dfdd87
Aung Naing Thu <aryalkrishna642@gmail.com>
2023-12-15 20:01:48 +0630

    Initial commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('wallet');
+            $table->string('spender')->nullable();
+            $table->string('balance')->default('0.0');
+            $table->string('real_balance')->default('0.0');
+            $table->enum('status',array('pending','approved'))->default('pending');
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
