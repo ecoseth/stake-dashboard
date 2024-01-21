60a9ab030718312e978230cd7e6e1ac34760eba0
kyawzinhtett <itskyawzinhtet@gmail.com>
2024-01-21 19:09:48 +0630

    chore: minor updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,7 @@
         <div class="content-wrapper">
 
             @yield('content')
-            
+
         </div>
 
         @include('layouts.footer')
@@ -43,11 +43,11 @@
     </div>
 
     <!-- jQuery -->
-    <script src="plugins/jquery/jquery.min.js"></script>
+    <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
     <!-- Bootstrap 4 -->
-    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <!-- AdminLTE App -->
-    <script src="dist/js/adminlte.min.js"></script>
+    <script src="{{ asset('dist/js/adminlte.min.js') }}"></script>
     @yield('scripts')
 
   </body>
