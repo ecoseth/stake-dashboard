f802ba83017dc9b8789c2917a73cbe4554c244f9
Aung Naing Thu <aryalkrishna642@gmail.com>
2024-04-04 23:07:53 +0700

    Fix code for wallet connect

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,11 +3,4 @@ import Wallet from "../js/components/Wallet.vue";
 
 const app = createApp(Wallet);
 
-app.use(
-    createWeb3Auth({
-        projectId: "d6eb491145ddbafe8af894199f6ff961",
-        chains: [Chains.sepolia,Chains.mainnet],
-    })
-);
-
 app.mount("#app");
